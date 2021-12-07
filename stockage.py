98cdbb7f4fe058379fe5119221cc15d43d88889f
AwoyDev <68854191+AwoyDev@users.noreply.github.com>
2021-12-07 19:58:08 +0100

    stockage

---
+++ stockage.py
@@ -0,0 +1,14 @@
+import shutil
+from progress.bar import Bar
+total, used, free = shutil.disk_usage("/")
+from math import ceil
+
+d = used / total
+eamount = ceil(100 * d)
+bar = Bar('\033[1mDisk size\033[m', max=100)
+for i in range(eamount):
+    bar.next()
+bar.finish()
+print("\033[1mTotal :\033[m %d Go" % (total // (2**30)))
+print("\033[1mUsed :\033[m %d Go" % (used // (2**30)))
+print("\033[1mRemaining :\033[m %d Go" % (free // (2**30)))
